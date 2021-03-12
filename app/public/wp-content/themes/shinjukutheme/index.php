8b36691902195bc0b3783bc78c621914f71237b3
Grant Holley <gvholley@gmail.com>
2021-03-12 17:41:00 +0900

    Edited loop

--- app/public/wp-content/themes/shinjukutheme/index.php
+++ app/public/wp-content/themes/shinjukutheme/index.php
@@ -2,7 +2,14 @@
 
   $names = array('Grant', 'Kaori', 'Leo', 'Donny');
 
+  $count = 0;
+
+  while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count].</li>";
+    $count++;
+  }
+
  ?>
 
 
-<p>Hi, my name is <?php echo $names[2] ?></p>
+
